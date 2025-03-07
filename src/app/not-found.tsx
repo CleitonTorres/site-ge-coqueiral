06cecc5709f5c6d2acfbd7104259210ae2e78f0d
Cleiton T. Machado <cleiton.t.machado@gmail.com>
2025-03-07 20:46:51 -0300

    add imagem na page not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
     return (
-      <div>
+      <div className="flexCollCenter">
         <h1>404 - Página não encontrada</h1>
         <p>A página que você tentou acessar não existe.</p>
-        <Link href="/">Voltar para a Home</Link>
+        <Image 
+          alt="not-found"
+          src={"/logo/not-found.png"}
+          width={300}
+          height={300}
+          style={{
+            borderRadius: '50%',
+            margin: 10
+          }}
+        />
+        <Link href="/" className="link">Voltar para a Home</Link>
       </div>
     );
   }
