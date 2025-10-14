bc52e2fa896201de9ad20ada9940483bc3c9667f
Cleiton T. Machado <cleiton.t.machado@gmail.com>
2025-10-14 14:57:05 -0300

    add componente head à page Aconteceu

--- src/app/aconteceu/page.tsx
+++ src/app/aconteceu/page.tsx
@@ -7,10 +7,20 @@ import { useContext } from 'react';
 import { Context } from '@/components/context/context';
 import { v4 } from 'uuid';
 import LoadIcon from '@/components/layout/loadIcon/loadIcon';
+import Head from 'next/head';
 
 export default function Page(){
     const context = useContext(Context);
     return(
+        <>
+        <Head>
+            <title>Aconteceu - Notícias</title>
+            <meta name="description" content="Fique por dentro das últimas notícias e eventos relacionados ao nosso trabalho e à comunidade que servimos. Acompanhe atualizações importantes, histórias inspiradoras e informações relevantes que impactam positivamente a vida das pessoas." />
+            <meta name="keywords" content="notícias, eventos, atualizações, comunidade, histórias inspiradoras, impacto social, trabalho voluntário, projetos sociais" />
+            <meta name="author" content="19 Grupo Escoteiro Coqueiral" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Section customClass={['flexCollTop', 'fullWidth']}>
             <div className={styles.conteiner}>
                 <h1 className={styles.title}>Notícias</h1>
@@ -46,5 +56,6 @@ export default function Page(){
                 </div>
             </div>
         </Section>
+        </>
     )
 }
